317acc2489c0c86b5728acc493d1e3b2c9f91196
Arclipse <arctale.work@gmail.com>
2022-08-26 22:46:38 +0200

    feat: added basic player script that can send move direction to server

--- scenes/player/Player.gd
+++ scenes/player/Player.gd
@@ -1,6 +1,59 @@
+extends KinematicBody2D
 
-# Called when the node enters the scene tree for the first time.
+var server = PacketPeerUDP.new()
+var serverIp = "127.0.0.1"
+var serverPort = 3000
+
+class Direction:
+	var north: bool = false
+	var south: bool = false
+	var west: bool = false
+	var east: bool = false
+	
 func _ready():
-	pass # Replace with function body.
+	connectServer()
+
+func _physics_process(delta):
+	move()
+
+func connectServer():
+	if server.listen(serverPort, serverIp) == OK:
+		print("Listening on port: " + str(serverPort) + " ip: " + str(serverIp))
+	else: 
+		print("Error trying to connect to server!")
+		
+
+func move():
+	var direction = getMoveDirection()
+	
+	print(direction.north)
+	
+func sendMovePacket(direction: Direction):
+	server.put_packet(direction)
+
+func getMoveDirection():
+	var direction: Direction = Direction.new()
+	
+	if Input.is_action_pressed("walk_north"):
+		direction.north = true
+	else:
+		direction.north = false
+	
+	if Input.is_action_pressed("walk_south"):
+		direction.south = true
+	else:
+		direction.south = false
+		
+	if Input.is_action_pressed("walk_west"):
+		direction.west = true
+	else:
+		direction.west = false
+	
+	if Input.is_action_pressed("walk_east"):
+		direction.east = true
+	else:
+		direction.east = false
+
+	return direction
 
 
