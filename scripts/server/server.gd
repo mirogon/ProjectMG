1821f6858db37c6c580b867ebfc2c31a7a2df9eb
Marcel Römer <m1smr@hotmail.com>
2022-08-27 12:53:36 +0200

    Bind port 0 and Send to 55580

--- scripts/server/server.gd
+++ scripts/server/server.gd
@@ -2,7 +2,7 @@ class_name ServerHandler
 
 var server = PacketPeerUDP.new()
 var serverIp = "127.0.0.1"
-var serverPort = 3000
+var serverPort = 0
 var peer = PacketPeerUDP.new()
 
 enum PacketType {
@@ -12,6 +12,7 @@ enum PacketType {
 func _init():
 	if server.bind(serverPort, serverIp) == OK:
 		print("Listening on port: " + str(serverPort) + " ip: " + str(serverIp))
+		server.connect_to_host("127.0.0.1", 55580)
 	else: 
 		print("Error trying to connect to server!")
 
@@ -19,4 +20,5 @@ func sendMovePacket(moveDirection: Vector2):
 	var packedByte = PackedByteArray([0, PacketType.move, int(moveDirection.x), int(moveDirection.y)])
 	server.put_packet(packedByte)
 	print(packedByte)
+	print("Sent packet")
 	
